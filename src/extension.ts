e15d4b1e030e2b3c3708c8203f27722bc866ce1a
Rafael Maiolla <rafaelmaiolla@gmail.com>
2016-03-04 23:26:15 -0300

    Save file to disk

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import Server from './Server';
+
+var server;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,16 +17,33 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+	let startSeverDispose = vscode.commands.registerCommand('extension.startServer', () => {
 		// The code you place here will be executed every time your command is executed
 
+    if (!server) {
+      server = new Server();
+    }
+
+    server.start();
+
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		// vscode.window.showInformationMessage('Hello World!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(startSeverDispose);
+
+  let stopSeverDispose = vscode.commands.registerCommand('extension.stopServer', () => {
+    if (server) {
+      server.stop();
+    }
+	});
+
+	context.subscriptions.push(stopSeverDispose);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+  if (server) {
+    server.stop();
+  }
 }
